09e457941a581a5582a42690d92f0ecdf6584dba
Wee Chuan <mr.ongwc@gmail.com>
2023-02-13 10:38:54 +0800

    first version

---
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.iss;
+
+import java.util.Random;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+       
+        Random rand = new Random();
+        int randinteger = 
+
+    }
+}
